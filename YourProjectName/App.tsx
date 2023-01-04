a889d140c2b3e623fcec9115e7d31c22a79ffdc9
VictorASantanan <victor.asantana012@gmail.com>
2023-01-04 16:59:24 -0300

    feat: adding eslint

--- YourProjectName/App.tsx
+++ YourProjectName/App.tsx
@@ -12,20 +12,13 @@ import React, {type PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const Section: React.FC<
   PropsWithChildren<{

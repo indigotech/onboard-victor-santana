a5f6b5d42b19bb668822bf5aba50e2e5cd4c5726
VictorASantanan <victor.asantana012@gmail.com>
2023-01-06 16:36:20 -0300

    feat? adding validation for signin fields

--- src/components/LoginFields.tsx
+++ src/components/LoginFields.tsx
@@ -1,13 +1,64 @@
-import React from 'react';
-import {Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, TextInput, View, Alert} from 'react-native';
 
 export const LoginFields = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+
+  const validateEmail = () => {
+    let regex = /^[\w-\.]{3,}@([\w-]{3,}\.)+(?:com)$/;
+    let isEmailValid = true;
+    if (email === '') {
+      setErrorEmail('Campo Email não pode ser vazio!');
+      isEmailValid = false;
+    } else if (!regex.test(email)) {
+      setErrorEmail('Formato de email inválido!');
+      isEmailValid = false;
+    }
+    return isEmailValid;
+  };
+
+  const validatePassword = () => {
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
+    let isPasswordValid = true;
+    if (password === '') {
+      setErrorPassword('Campo Senha não pode ser vazio!');
+      isPasswordValid = false;
+    } else if (!regex.test(password)) {
+      setErrorPassword('Formato de senha inválido!');
+      isPasswordValid = false;
+    }
+    return isPasswordValid;
+  };
+
+  const validate = () => {
+    console.log(validateEmail());
+    if (!validateEmail()) {
+      return Alert.alert(errorEmail);
+    } else if (!validatePassword()) {
+      return Alert.alert(errorPassword);
+    } else {
+      return Alert.alert('Sucesso!');
+    }
+  };
+
   return (
     <View>
       <Text>Email</Text>
-      <TextInput placeholder="digite seu email" />
+      <TextInput
+        placeholder="digite seu email"
+        onChangeText={value => setEmail(value)}
+        keyboardType="email-address"
+      />
       <Text>Senha</Text>
-      <TextInput placeholder="digite sua senha" />
+      <TextInput
+        placeholder="digite sua senha"
+        onChangeText={key => setPassword(key)}
+        secureTextEntry={true}
+      />
+      <Button title="Entrar" onPress={() => validate()} />
     </View>
   );
 };
--- src/signin/Signin.tsx
+++ src/signin/Signin.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import {Button, SafeAreaView, Text} from 'react-native';
 import {LoginFields} from '../components/LoginFields';
+import {SafeAreaView, Text} from 'react-native';
 
 export const LoginScreen = () => {
   return (
     <SafeAreaView>
       <Text>Bem vindo(a) à Taqtile!</Text>
       <LoginFields />
-      <Button title="Entrar" />
     </SafeAreaView>
   );
 };

5c9c62b8864156723c1e9c49b0628ab215bff144
VictorASantanan <victor.asantana012@gmail.com>
2023-01-11 17:55:51 -0300

    feat: adding a basic user list on home

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {FlatList, SafeAreaView, Text, View} from 'react-native';
+
+interface User {
+  userName: string;
+  userEmail: string;
+}
+
+const userList: User[] = [
+  {
+    userName: 'joao12',
+    userEmail: 'joao12@taqtile.com.br',
+  },
+  {
+    userName: 'guilherme2001',
+    userEmail: 'gui2001@taqtile.com.br',
+  },
+  {
+    userName: 'leth',
+    userEmail: 'leticia@taqtile.com.br',
+  },
+  {
+    userName: 'cecilia',
+    userEmail: 'cecilia@taqtile.com.br',
+  },
+];
 
 export const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <Text>Lista de usuários: </Text>
+      <FlatList
+        data={userList}
+        renderItem={({item}) => (
+          <View>
+            <Text>Nome: {item.userName}</Text>
+            <Text>Email: {item.userEmail}</Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };

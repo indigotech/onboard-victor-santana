d84a81743f68137eeb78f5a336611ca2aaba1e06
VictorASantanan <victor.asantana012@gmail.com>
2023-01-12 10:27:35 -0300

    fix: implementing requested changes

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
 import {registerScreens} from './src/utils/navigation';
 import {Navigation} from 'react-native-navigation';
-
-AppRegistry.registerComponent(appName, () => App);
 
 registerScreens();
 

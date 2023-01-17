3a2bd02965ab1f259b7a8acf220e3c39a41d165d
VictorASantanan <victor.asantana012@gmail.com>
2023-01-17 15:15:55 -0300

    feat: adding fields validation

--- src/add-user/add-user.tsx
+++ src/add-user/add-user.tsx
@@ -1,6 +1,76 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Button, Text, TextInput, View} from 'react-native';
+import {
+  validateBirthDate,
+  validateEmail,
+  validateName,
+  validatePassword,
+  validatePhone,
+} from '../utils/validation';
 
 export const AddUserScreen = () => {
-  return <Text>Adicionar usuário</Text>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [phone, setPhone] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+
+  const validateUserData = () => {
+    const emailValidation = validateEmail(email);
+    const passwordValidation = validatePassword(password);
+    const phoneValidation = validatePhone(phone);
+    const nameValidation = validateName(name);
+    const birthDateValidation = validateBirthDate(birthDate);
+    if (emailValidation !== '') {
+      return Alert.alert(emailValidation);
+    }
+
+    if (passwordValidation !== '') {
+      return Alert.alert(passwordValidation);
+    } else if (password !== confirmPassword) {
+      return Alert.alert('As senhas devem ser iguais!');
+    }
+
+    if (phoneValidation !== '') {
+      return Alert.alert(phoneValidation);
+    }
+
+    if (nameValidation !== '') {
+      return Alert.alert(nameValidation);
+    }
+
+    if (birthDateValidation !== '') {
+      return Alert.alert(birthDateValidation);
+    }
+
+    return Alert.alert('Sucesso!');
+  };
+
+  return (
+    <View>
+      <Text>Adicionar Usuário</Text>
+      <Text>Nome</Text>
+      <TextInput placeholder="nome" onChangeText={setName} />
+      <Text>Email</Text>
+      <TextInput placeholder="email" onChangeText={setEmail} />
+      <Text>Senha</Text>
+      <TextInput
+        placeholder="senha"
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+      <Text>Confirmar Senha</Text>
+      <TextInput
+        placeholder="senha"
+        onChangeText={setConfirmPassword}
+        secureTextEntry={true}
+      />
+      <Text>Telefone</Text>
+      <TextInput placeholder="telefone" onChangeText={setPhone} />
+      <Text>Data de Nascimento</Text>
+      <TextInput placeholder="data de nascimento" onChangeText={setBirthDate} />
+      <Button title="Adicionar Usuário" onPress={() => validateUserData()} />
+    </View>
+  );
 };
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,5 +1,6 @@
 const regexEmail = /.+[@].+\.com/;
 const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
+const regexBirthDate = /(^\d{4}\/\d{2}\/\d{2}$)|(^\d{4}\-\d{2}\-\d{2}$)/;
 
 export const validateEmail = (email: string) => {
   if (email === '') {
@@ -16,5 +17,41 @@ export const validatePassword = (password: string) => {
   } else if (!regexPassword.test(password)) {
     return 'Formato de senha inválido!';
   }
+  return '';
+};
+
+export const validatePhone = (phone: string) => {
+  if (phone === '') {
+    return 'Campo telefone não pode ser vazio!';
+  } else if (phone.length < 11) {
+    return 'Formato de telefone inválido';
+  }
+  return '';
+};
+
+export const validateName = (name: string) => {
+  if (name === '') {
+    return 'Campo nome não pode ser vazio!';
+  }
+  return '';
+};
+
+export const validateBirthDate = (birthDate: string) => {
+  const today = new Date();
+  const verifyBirthDate = new Date(birthDate);
+  const minDate = new Date('1905-01-01');
+
+  if (!regexBirthDate.test(birthDate)) {
+    return 'Formato de data inválido!';
+  }
+
+  if (today < verifyBirthDate) {
+    return 'Data de nascimento no futuro';
+  }
+
+  if (verifyBirthDate < minDate) {
+    return 'Data de nascimento muito antiga!';
+  }
+
   return '';
 };
